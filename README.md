9107f604340bef4d008c4b8a98c64765d3a45f6d
Kodzima <105111017+kod3ima@users.noreply.github.com>
2023-09-27 16:40:09 +1000

    Added list tasks

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Setup
 
 - docker build -t bot_name .
-- docker run -e BOT_TOKEN=your_token -d --name telegram_container telegram_container
+- docker run -e BOT_TOKEN=your_token -d --name image_name container_name
--- main.py
+++ main.py
@@ -4,12 +4,35 @@ from telebot import TeleBot
 
 
 load_dotenv()
+
 bot = TeleBot(os.getenv('BOT_TOKEN'))
+tasks = list()
 
 @bot.message_handler(commands=['start'])
 def start(message):
     text = 'Welcome to my bot from docker'
     bot.send_message(message.chat.id, text)
+
+@bot.message_handler(commands=['get_tasks'])
+def get_tasks(message):
+    text = ''
+    for task in tasks:
+        text += (f"User: {task['chat_id']}\n\n"
+                f"Title: {task['title']}\n\n"
+                f"Task: {task['text']}\n\n")
+
+    bot.send_message(message.chat.id, text)
+
+@bot.message_handler(func=lambda message: True)
+def add_task(message: dict):
+    command, title, task = message.text.split('\n\n')
+
+    if command == 'add':
+        tasks.append({
+            'chat_id': message.chat.id,
+            'title': title,
+            'text': task
+        })
 
 if __name__ == '__main__':
     bot.polling()
